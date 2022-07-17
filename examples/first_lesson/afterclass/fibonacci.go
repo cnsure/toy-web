4fa30daac180c48c12b3613f0c3c2cc0ef08cad1
Yezi <yeziupwang@gmail.com>
2022-07-17 22:54:30 +0800

    homework

--- examples/first_lesson/afterclass/fibonacci.go
+++ examples/first_lesson/afterclass/fibonacci.go
@@ -1,10 +1,12 @@
 package main
 
 func main() {
-
+	println(fibonacci(2))
 }
 
 func fibonacci(n int) int {
-	// TODO
-	return 0
+	if n == 1 || n == 0 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
 }
--- examples/first_lesson/afterclass/fmt.go
+++ examples/first_lesson/afterclass/fmt.go
@@ -1,14 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	println(printNumWith2(3.1415))
+	println(printBytes([]byte{97, 98}))
 }
 
 // 输出两位小数
 func printNumWith2(float642 float64) string {
-	return ""
+	return fmt.Sprintf("%.2f", float642)
 }
 
 func printBytes(data []byte) string {
-	return ""
+	return fmt.Sprintf("%s", data)
 }
